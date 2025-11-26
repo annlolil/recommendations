599a2e40c38dce8715e6dc265398dd1c9efcbdd5
Ann-Louis Lilja <lilja.annlouis@gmail.com>
2025-11-26 09:56:04 +0100

    fix: changed requestmapping to /api/v1/recommendations

--- src/main/java/com/example/recommendations/controllers/RecommendationsController.java
+++ src/main/java/com/example/recommendations/controllers/RecommendationsController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/recommendations")
 public class RecommendationsController {
 
     private final RecommendationsService recommendationsService;
